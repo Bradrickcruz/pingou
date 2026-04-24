1ea0b4961ec5528462532426dcf29c240aac05e0
Bryan H Cruz <bryanhcruz@outlook.com>
2026-04-24 03:50:10 -0300

    refactor main function for improved configuration and database handling

--- cmd/pingou/main.go
+++ cmd/pingou/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"log/slog"
 	"os"
+
+	"github.com/Bradrickcruz/pingou/internal/config"
+	"github.com/Bradrickcruz/pingou/internal/database"
 )
 
 var (
@@ -17,9 +20,20 @@ func main() {
 	}))
 	slog.SetDefault(logger)
 
-	slog.Info("Pingou starting",
-		"version", version,
-		"commit", commit,
-		"buildDate", buildDate,
-	)
+	slog.Info("Pingou starting", "version", version, "commit", commit, "buildDate", buildDate)
+
+	cfg, err := config.Load()
+	if err != nil {
+		slog.Error("config error", "err", err)
+		os.Exit(1)
+	}
+
+	db, err := database.Open(cfg.DatabaseURL)
+	if err != nil {
+		slog.Error("database error", "err", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	slog.Info("ready")
 }
