9b4f7b7b3a4b11bd756b2bb5de39c60042a799fd
Bryan H Cruz <bryanhcruz@outlook.com>
2026-04-24 03:26:25 -0300

    update logging to include version, commit, and build date information

--- cmd/pingou/main.go
+++ cmd/pingou/main.go
@@ -5,11 +5,21 @@ import (
 	"os"
 )
 
+var (
+	version   = "dev"
+	commit    = "none"
+	buildDate = "unknown"
+)
+
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelInfo,
 	}))
 	slog.SetDefault(logger)
 
-	slog.Info("Pingou starting", "version", "0.1.0")
+	slog.Info("Pingou starting",
+		"version", version,
+		"commit", commit,
+		"buildDate", buildDate,
+	)
 }
